466bc52c29c8e62373dc2abdbe4296476fbfd3cc
Hamed Jahan <iamhamedjahan@gmail.com>
2024-04-13 11:26:40 +0330

    handle authorization and form validation

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,27 @@
-const setCookie = (data) => {
-  document.cookie = `token=${data} ; max-age=${24 * 60 * 60}; path=/`;
+const errors = document.querySelectorAll("#error");
+
+const validateUserName = (username) => {
+  const regex = /^[a-zA-Z\d_]{4,16}$/;
+  const result = regex.test(username);
+  return result;
 };
 
-export { setCookie };
+const validatePassword = (password) => {
+  const regex = /^.{4,20}$/;
+  const result = regex.test(password);
+  return result;
+};
+
+const validateForm = (username, password) => {
+  const usernameResult = validateUserName(username);
+  const passwordResult = validatePassword(password);
+  if (usernameResult && passwordResult) {
+    return true;
+  } else if (!usernameResult) {
+    errors[0].innerText = "User Name isn't correct";
+  } else if (!passwordResult) {
+    errors[1].innerText = "Should be between 4 and 20 characters";
+  }
+};
+
+export default validateForm;
